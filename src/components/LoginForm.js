cc6105df9f80790e052efcafc27d62e45eca57ed
shams220 <103259015+shams220@users.noreply.github.com>
2025-12-05 09:11:58 +0530

    Don

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,12 @@
 import React from "react";
 import './form.css';
 const LoginForm = ({isLoggedIn,setIsLoggedIn}) => {
-const login=()=>{
+const login=(e)=>{
+    e.prevent.default()
     
     setIsLoggedIn(true)
-console.log("isLoggedIn",props.prop[0])
-console.log("isLoggedIn",props.prop[1])
+// console.log("isLoggedIn",props.prop[0])
+// console.log("isLoggedIn",props.prop[1])
 
 }
 
@@ -13,7 +14,8 @@ return (
 
 <div>
    
-{isLoggedIn ? <p>You are logged in!</p> :   <div>
+{isLoggedIn ? <p>You are logged in!</p> :  
+ <form onSubmit={login}>
       <div className="username">
         <label htmlFor="">Username:</label>
         <input type="text" />
@@ -22,8 +24,8 @@ return (
         <label htmlFor="">Password:</label>
         <input type="password" />
       </div>
-      <button onClick={login}>Login</button>
-    </div> 
+     <button type='submit'>Login</button>
+    </form> 
 }</div>
 
 
